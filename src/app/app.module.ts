b502f42dc3ec91306b569bf9ef801a110bb966b8
vilic <i@vilic.info>
2017-08-16 08:43:42 +0800

    Minimal repro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, UrlSegment } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  @NgModule({
+    declarations: [
+      AppComponent,
+      TestComponent
+    ],
+    imports: [
+      BrowserModule,
+      RouterModule.forRoot([
+        {
+          matcher(segments: UrlSegment[]) {
+            if (segments.length) {
+              return {
+                consumed: [segments[0]],
+              };
+            } else {
+              return undefined;
+            }
+          },
+          component: TestComponent,
+        }
+      ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
